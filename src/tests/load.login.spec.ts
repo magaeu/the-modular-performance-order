b0f7baa94363f353c1af4e2ab50930ec4eb748f4
Marcela Oliveira <magaeu@gmail.com>
2026-06-19 06:26:06 +0000

    create avg load login

---
+++ src/tests/load.login.spec.ts
@@ -0,0 +1,41 @@
+import { check, sleep } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.05'],
+    http_req_duration: ['p(95)<1000'],
+  },
+  scenarios: {
+    average_load: {
+      executor: 'ramping-vus',
+      stages: [
+        { duration: '10s', target: 20 },
+        { duration: '50s', target: 20 },
+        { duration: '5s', target: 0 },
+      ],
+    },
+  },
+  summaryTrendStats: ['avg','min','max','count']
+};
+
+export default function () {
+  const url = 'https://quickpizza.grafana.com/api/users/token/login';
+  const payload = JSON.stringify({
+    username: 'default',
+    password: '1234',
+  });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const loginResponse = http.post(url, payload, params);
+
+  check(loginResponse, {
+    'response code status was 200': (resp) => resp.status == 200,
+  });
+  sleep(1);
+
+}
